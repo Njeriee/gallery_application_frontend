cd16ec32f023447df7afe7211c8b26a7b189857f
sav <savelynnwachira@gmail.com>
2023-02-03 22:42:14 +0300

    nav bar and dashboard page updated

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
-  <nav class="nav flex flex-wrap items-center justify-between px-4 bg-amber-500 text-white">
+  <nav
+    class="nav flex flex-wrap items-center justify-between px-4 bg-amber-500 text-white"
+  >
     <div class="flex flex-no-shrink items-center mr-6 py-3 text-grey-darkest">
-      <svg
-        class="h-8 mr-2 w-8"
-        xmlns="http://www.w3.org/2000/svg"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-miterlimit="1.5"
-        clip-rule="evenodd"
-        viewBox="0 0 716 895"
-      >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+      class="h-10 w-10 mx-2 " fill="current">
+        <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
         <path
-          d="M357.776 0l357.77 178.885v536.657l-357.77 178.89L0 715.542V178.885"
-        ></path>
-        <path
-          class="text-black fill-current"
-          d="M357.776 804.982l268.32-134.16v-178.89l-89.44-44.72 89.44-44.72V223.606L357.776 89.442v626.1l-178.89-89.44V178.885l-89.443 44.721v447.216l268.333 134.16z"
-        ></path>
-        <path
-          d="M447.216 670.822l89.44-44.72v-89.45l-89.44-44.72v178.89zM447.216 402.492l89.44-44.721v-89.443l-89.44-44.722"
-        ></path>
+          d="M512 32H160c-35.35 0-64 28.65-64 64v224c0 35.35 28.65 64 64 64H512c35.35 0 64-28.65 64-64V96C576 60.65 547.3 32 512 32zM528 320c0 8.822-7.178 16-16 16h-16l-109.3-160.9C383.7 170.7 378.7 168 373.3 168c-5.352 0-10.35 2.672-13.31 7.125l-62.74 94.11L274.9 238.6C271.9 234.4 267.1 232 262 232c-5.109 0-9.914 2.441-12.93 6.574L176 336H160c-8.822 0-16-7.178-16-16V96c0-8.822 7.178-16 16-16H512c8.822 0 16 7.178 16 16V320zM224 112c-17.67 0-32 14.33-32 32s14.33 32 32 32c17.68 0 32-14.33 32-32S241.7 112 224 112zM456 480H120C53.83 480 0 426.2 0 360v-240C0 106.8 10.75 96 24 96S48 106.8 48 120v240c0 39.7 32.3 72 72 72h336c13.25 0 24 10.75 24 24S469.3 480 456 480z"
+        />
       </svg>
-      <span class="font-semibold text-xl tracking-tight"><router-link to="/">View Albums App</router-link></span>
+      <span class="font-semibold text-xl tracking-tight"
+        ><router-link to="/">View Albums App</router-link></span
+      >
     </div>
 
     <input class="menu-btn hidden" type="checkbox" id="menu-btn" />
@@ -32,28 +23,46 @@
       <span class="navicon bg-grey-darkest flex items-center relative"></span>
     </label>
 
-    <ul class="menu"  
-    >
-      <span class="md:border-none flex  list-reset m-0  md:w-auto"  v-if="isLoggedIn">
-        <li class="mx-4 md:border-none py-2  flex  justify-end list-reset m-0 w-full md:w-auto">
+    <ul class="menu">
+      <span
+        class="md:border-none flex list-reset m-0 md:w-auto"
+        v-if="isLoggedIn"
+      >
+        <li
+          class="mx-4 md:border-none py-2 flex justify-end list-reset m-0 w-full md:w-auto"
+        >
           <router-link to="/admin">Dashboard</router-link>
         </li>
-        <li class="mx-4 md:border-none flex  justify-end list-reset m-0 w-full md:w-auto">
-          <button class="rounded-lg bg-black px-4 py-2 text-amber-400 hover:animate-bounce" @click="signOut">Logout</button>
+        <li
+          class="mx-4 md:border-none flex justify-end list-reset m-0 w-full md:w-auto"
+        >
+          <button
+            class="rounded-lg bg-black px-4 py-2 text-amber-400 hover:animate-bounce"
+            @click="signOut"
+          >
+            Logout
+          </button>
         </li>
       </span>
-      <span class="md:border-none flex justify-end list-reset m-0 w-full md:w-auto" v-else>
-        <li class="mx-4 md:border-none rounded-lg bg-black px-4 py-2 text-amber-400 hover:animate-bounce">
+      <span
+        class="md:border-none flex justify-end list-reset m-0 w-full md:w-auto"
+        v-else
+      >
+        <li
+          class="mx-4 md:border-none rounded-lg bg-black px-4 py-2 text-amber-400 hover:animate-bounce"
+        >
           <router-link to="/login">sign in</router-link>
         </li>
-        <li class="md:border-none rounded-lg bg-black px-4 py-2 text-amber-400 hover:animate-bounce">
+        <li
+          class="md:border-none rounded-lg bg-black px-4 py-2 text-amber-400 hover:animate-bounce"
+        >
           <router-link to="/signup">sign up</router-link>
         </li>
       </span>
     </ul>
   </nav>
   <router-view />
-  <footer class="py-4 bg-black text-white">
+  <footer class="py-4 bg-amber-500 text-white">
     <p class="text-center">@2023</p>
   </footer>
 </template>
@@ -90,7 +99,7 @@ nav {
 @media (max-width: 767px) {
   .navicon {
     width: 1.125em;
-    height: .125em;
+    height: 0.125em;
   }
 
   .navicon::before,
@@ -99,9 +108,9 @@ nav {
     position: absolute;
     width: 100%;
     height: 100%;
-    transition: all .2s ease-out;
-    content: '';
-    background: #3D4852;
+    transition: all 0.2s ease-out;
+    content: "";
+    background: #3d4852;
   }
 
   .navicon::before {
--- src/components/AdminPage.vue
+++ src/components/AdminPage.vue
@@ -1,5 +1,5 @@
 <template>
-     <div class="flex h-screen">
+     <div class="flex w-screen h-screen">
             <div class="px-4 py-2 bg-black lg:w-1/4">
                 <svg xmlns="http://www.w3.org/2000/svg" class="inline w-8 h-8 text-white lg:hidden" fill="none"
                     viewBox="0 0 24 24" stroke="currentColor">
@@ -10,30 +10,14 @@
                         <h1 class="text-2xl font-bold text-white">Admin Dashboard</h1>
                     </div>
                     <ul>
-                        <li class="mb-6">
-                            <div class="relative">
-                                <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-                                    <button type="submit" class="p-1 focus:outline-none">
-                                        <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                                            stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
-                                            class="w-4 h-4 text-amber-500">
-                                            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                        </svg>
-                                    </button>
-                                </span>
-                                <input type="search" name="search"
-                               class="w-full px-4 py-2 pl-12 rounded shadow outline-none" placeholder="Search...">
-                        
-                            </div>
-                        </li>
-                        <li class="mb-2 rounded hover:shadow hover:bg-gray-800">
+                       <li class="mb-2 rounded hover:shadow hover:bg-gray-800">
                             <a href="#" class="inline-block w-full h-full px-3 py-2 font-bold text-white hover:text-amber-500">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-6 h-6 mr-2 -mt-2 text-amber-500"
                                     fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                                 </svg>
-                                Home
+                                <router-link to="/">Home</router-link>
                             </a>
                         </li>
                         <li class="mb-2 bg-gray-800 rounded shadow">
@@ -101,7 +85,7 @@
                                 </svg>
                             </div>
                             <div class="mx-4">
-                                <h4 class="text-2xl font-semibold text-gray-700">1000</h4>
+                                <h4 class="text-2xl font-semibold text-gray-700">50000</h4>
                                 <div class="text-gray-500">All Photos</div>
                             </div>
                         </div>
